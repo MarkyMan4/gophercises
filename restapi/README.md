9a82b1f8451807f62cc39c3bdffd0c14f831a960
MarkyMan4 <markus.hutnik@gmail.com>
2022-05-23 23:28:51 -0500

    basic web server

---
+++ restapi/README.md
@@ -0,0 +1,5 @@
+# HTTP Server
+
+This is a basic HTTP server using only the Go standard library.
+
+There are examples of returning HTML or JSON.
---
+++ restapi/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Dog struct {
+	Breed      string `json:"breed"`
+	Size       string `json:"size"`
+	Temperment string `json:"temperment`
+}
+
+// returning HTML
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>hello</h1>"))
+}
+
+func GetDog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	dog := Dog{
+		Breed:      "Shepadoodle",
+		Size:       "Medium",
+		Temperment: "Friendly",
+	}
+
+	json.NewEncoder(w).Encode(dog)
+}
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/dog", GetDog)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}

aebf089c60c1396da3245299ed6288328bc9c0f9
MarkyMan4 <markus.hutnik@gmail.com>
2022-06-02 00:09:17 -0500

    get weather data and print to std out

--- pipeline/main.go
+++ pipeline/main.go
@@ -8,16 +8,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"reflect"
+	"net/http"
+	"os"
 )
 
-func main() {
-	sec, err := GetSecret("lat")
+func getKeyAndCoords() (interface{}, interface{}, interface{}, error) {
+	var err error
+	apiKey, apiKeyErr := GetSecret("apiKey")
+	lat, latErr := GetSecret("lat")
+	lon, lonErr := GetSecret("lon")
 
-	if err != nil {
-		log.Fatalln("secret does not exist")
+	if apiKeyErr != nil || latErr != nil || lonErr != nil {
+		err = os.ErrNotExist
 	}
 
-	fmt.Println(reflect.TypeOf(sec))
+	return apiKey, lat, lon, err
+}
+
+func main() {
+	apiKey, lat, lon, secretErr := getKeyAndCoords()
+
+	if secretErr != nil {
+		log.Fatalln("failed to get secrets")
+	}
+
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s", lat, lon, apiKey)
+	res, reqErr := http.Get(url)
+
+	if reqErr != nil {
+		log.Fatalln("failed to make request")
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+
+	if readErr != nil {
+		log.Fatalln("failed to read response body")
+	}
+
+	fmt.Println(string(body))
 }

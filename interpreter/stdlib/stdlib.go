2dbd667ec3cad6a6203138bb76215ebe08a0345a
MarkyMan4 <markus.hutnik@gmail.com>
2022-09-28 19:23:43 -0500

    added length function for strings and arrays

--- interpreter/stdlib/stdlib.go
+++ interpreter/stdlib/stdlib.go
@@ -11,6 +11,7 @@ type BuiltIn func(args ...object.Object) object.Object
 var BuiltInFuns = map[string]BuiltIn{
 	"print":  PrintFun,
 	"substr": SubstringFun,
+	"length": LengthFun,
 }
 
 func PrintFun(args ...object.Object) object.Object {
@@ -56,4 +57,22 @@ func SubstringFun(args ...object.Object) object.Object {
 	endIdx := args[2].(*object.IntegerObject).Value
 
 	return &object.StringObject{Value: strLit[startIdx:endIdx]}
+}
+
+func LengthFun(args ...object.Object) object.Object {
+	if args[0].Type() != object.STRING_OBJ && args[0].Type() != object.ARRAY_OBJ {
+		return &object.ErrorObject{Message: fmt.Sprintf("object of type %s has no function length", args[0].Type())}
+	}
+
+	if len(args) > 1 {
+		return &object.ErrorObject{Message: "length function takes no arguments"}
+	}
+
+	if args[0].Type() == object.STRING_OBJ {
+		strObj := args[0].(*object.StringObject)
+		return &object.IntegerObject{Value: int64(len(strObj.Value))}
+	} else {
+		arrObj := args[0].(*object.ArrayObject)
+		return &object.IntegerObject{Value: int64(len(arrObj.Items))}
+	}
 }
